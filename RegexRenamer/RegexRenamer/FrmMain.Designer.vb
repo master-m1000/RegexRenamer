0da0871f19b7c3749a140195675ffb933f11a13c
master-m1000 <mario.wagenknecht@gmail.com>
2016-08-06 06:43:36 +0200

    First commit

---
+++ RegexRenamer/RegexRenamer/FrmMain.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LblDirectory = New System.Windows.Forms.Label()
+        Me.TbDirectory = New System.Windows.Forms.TextBox()
+        Me.BtnDirectory = New System.Windows.Forms.Button()
+        Me.LblLookupPattern = New System.Windows.Forms.Label()
+        Me.LblReplacePattern = New System.Windows.Forms.Label()
+        Me.TbLookupPattern = New System.Windows.Forms.TextBox()
+        Me.TbReplacePattern = New System.Windows.Forms.TextBox()
+        Me.BtnRename = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.TsPbStatus = New System.Windows.Forms.ToolStripProgressBar()
+        Me.TsLblStatus = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.LvPreview = New System.Windows.Forms.ListView()
+        Me.ChOldName = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ChNewName = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.TsDdBtnAbout = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GoToGitHubToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LicenseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'LblDirectory
+        '
+        Me.LblDirectory.AutoSize = True
+        Me.LblDirectory.Location = New System.Drawing.Point(12, 15)
+        Me.LblDirectory.Name = "LblDirectory"
+        Me.LblDirectory.Size = New System.Drawing.Size(56, 13)
+        Me.LblDirectory.TabIndex = 0
+        Me.LblDirectory.Text = "&Directory:"
+        '
+        'TbDirectory
+        '
+        Me.TbDirectory.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TbDirectory.Location = New System.Drawing.Point(74, 12)
+        Me.TbDirectory.Name = "TbDirectory"
+        Me.TbDirectory.Size = New System.Drawing.Size(568, 22)
+        Me.TbDirectory.TabIndex = 1
+        '
+        'BtnDirectory
+        '
+        Me.BtnDirectory.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnDirectory.Location = New System.Drawing.Point(648, 12)
+        Me.BtnDirectory.Name = "BtnDirectory"
+        Me.BtnDirectory.Size = New System.Drawing.Size(24, 23)
+        Me.BtnDirectory.TabIndex = 2
+        Me.BtnDirectory.Text = "..."
+        Me.BtnDirectory.UseVisualStyleBackColor = True
+        '
+        'LblLookupPattern
+        '
+        Me.LblLookupPattern.AutoSize = True
+        Me.LblLookupPattern.Location = New System.Drawing.Point(12, 43)
+        Me.LblLookupPattern.Name = "LblLookupPattern"
+        Me.LblLookupPattern.Size = New System.Drawing.Size(90, 13)
+        Me.LblLookupPattern.TabIndex = 3
+        Me.LblLookupPattern.Text = "&Lookup pattern:"
+        '
+        'LblReplacePattern
+        '
+        Me.LblReplacePattern.AutoSize = True
+        Me.LblReplacePattern.Location = New System.Drawing.Point(12, 69)
+        Me.LblReplacePattern.Name = "LblReplacePattern"
+        Me.LblReplacePattern.Size = New System.Drawing.Size(91, 13)
+        Me.LblReplacePattern.TabIndex = 4
+        Me.LblReplacePattern.Text = "&Replace pattern:"
+        '
+        'TbLookupPattern
+        '
+        Me.TbLookupPattern.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TbLookupPattern.Location = New System.Drawing.Point(109, 40)
+        Me.TbLookupPattern.Name = "TbLookupPattern"
+        Me.TbLookupPattern.Size = New System.Drawing.Size(563, 22)
+        Me.TbLookupPattern.TabIndex = 5
+        '
+        'TbReplacePattern
+        '
+        Me.TbReplacePattern.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TbReplacePattern.Location = New System.Drawing.Point(109, 68)
+        Me.TbReplacePattern.Name = "TbReplacePattern"
+        Me.TbReplacePattern.Size = New System.Drawing.Size(563, 22)
+        Me.TbReplacePattern.TabIndex = 6
+        '
+        'BtnRename
+        '
+        Me.BtnRename.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnRename.Location = New System.Drawing.Point(12, 96)
+        Me.BtnRename.Name = "BtnRename"
+        Me.BtnRename.Size = New System.Drawing.Size(660, 23)
+        Me.BtnRename.TabIndex = 7
+        Me.BtnRename.Text = "Rename"
+        Me.BtnRename.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TsDdBtnAbout, Me.TsPbStatus, Me.TsLblStatus})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 299)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(684, 22)
+        Me.StatusStrip1.TabIndex = 8
+        '
+        'TsPbStatus
+        '
+        Me.TsPbStatus.Name = "TsPbStatus"
+        Me.TsPbStatus.Size = New System.Drawing.Size(100, 16)
+        '
+        'TsLblStatus
+        '
+        Me.TsLblStatus.Name = "TsLblStatus"
+        Me.TsLblStatus.Size = New System.Drawing.Size(50, 17)
+        Me.TsLblStatus.Text = "Loading"
+        '
+        'FolderBrowserDialog1
+        '
+        Me.FolderBrowserDialog1.Description = "Please select a directory."
+        Me.FolderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer
+        '
+        'LvPreview
+        '
+        Me.LvPreview.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LvPreview.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ChOldName, Me.ChNewName})
+        Me.LvPreview.GridLines = True
+        Me.LvPreview.Location = New System.Drawing.Point(12, 125)
+        Me.LvPreview.Name = "LvPreview"
+        Me.LvPreview.Size = New System.Drawing.Size(660, 171)
+        Me.LvPreview.TabIndex = 9
+        Me.LvPreview.UseCompatibleStateImageBehavior = False
+        Me.LvPreview.View = System.Windows.Forms.View.Details
+        '
+        'ChOldName
+        '
+        Me.ChOldName.Text = "old name"
+        Me.ChOldName.Width = 128
+        '
+        'ChNewName
+        '
+        Me.ChNewName.Text = "new name"
+        Me.ChNewName.Width = 128
+        '
+        'TsDdBtnAbout
+        '
+        Me.TsDdBtnAbout.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.TsDdBtnAbout.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.TsDdBtnAbout.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.GoToGitHubToolStripMenuItem, Me.ToolStripSeparator1, Me.LicenseToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.TsDdBtnAbout.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.TsDdBtnAbout.Name = "TsDdBtnAbout"
+        Me.TsDdBtnAbout.Size = New System.Drawing.Size(53, 20)
+        Me.TsDdBtnAbout.Text = "About"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'GoToGitHubToolStripMenuItem
+        '
+        Me.GoToGitHubToolStripMenuItem.Name = "GoToGitHubToolStripMenuItem"
+        Me.GoToGitHubToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.GoToGitHubToolStripMenuItem.Text = "Go to GitHub"
+        '
+        'LicenseToolStripMenuItem
+        '
+        Me.LicenseToolStripMenuItem.Name = "LicenseToolStripMenuItem"
+        Me.LicenseToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.LicenseToolStripMenuItem.Text = "License"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(149, 6)
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(684, 321)
+        Me.Controls.Add(Me.LvPreview)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.BtnRename)
+        Me.Controls.Add(Me.TbReplacePattern)
+        Me.Controls.Add(Me.TbLookupPattern)
+        Me.Controls.Add(Me.LblReplacePattern)
+        Me.Controls.Add(Me.LblLookupPattern)
+        Me.Controls.Add(Me.BtnDirectory)
+        Me.Controls.Add(Me.TbDirectory)
+        Me.Controls.Add(Me.LblDirectory)
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!)
+        Me.MinimumSize = New System.Drawing.Size(300, 270)
+        Me.Name = "FrmMain"
+        Me.Text = "RegexRenamer"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LblDirectory As Label
+    Friend WithEvents TbDirectory As TextBox
+    Friend WithEvents BtnDirectory As Button
+    Friend WithEvents LblLookupPattern As Label
+    Friend WithEvents LblReplacePattern As Label
+    Friend WithEvents TbLookupPattern As TextBox
+    Friend WithEvents TbReplacePattern As TextBox
+    Friend WithEvents BtnRename As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents TsLblStatus As ToolStripStatusLabel
+    Friend WithEvents TsPbStatus As ToolStripProgressBar
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents LvPreview As ListView
+    Friend WithEvents ChOldName As ColumnHeader
+    Friend WithEvents ChNewName As ColumnHeader
+    Friend WithEvents TsDdBtnAbout As ToolStripDropDownButton
+    Friend WithEvents GoToGitHubToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents LicenseToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ RegexRenamer/RegexRenamer/FrmMain.vb
@@ -0,0 +1,154 @@
+﻿Public Class FrmMain
+    Dim fileList As New List(Of String)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Icon = My.Resources.Icon
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        SetStatus("Ready", 0)
+    End Sub
+
+    Private Sub BtnDirectory_Click(sender As Object, e As EventArgs) Handles BtnDirectory.Click
+        If IO.Directory.Exists(TbDirectory.Text) = True Then
+            FolderBrowserDialog1.SelectedPath = TbDirectory.Text
+        End If
+        If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
+            TbDirectory.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub BtnRename_Click(sender As Object, e As EventArgs) Handles BtnRename.Click
+        If String.IsNullOrEmpty(TbLookupPattern.Text) = False And String.IsNullOrEmpty(TbReplacePattern.Text) = False Then
+            If MessageBox.Show(Me, "Are you sure to continue?" & vbNewLine & "That is the last chance to cancel!", "Continue?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) = DialogResult.Yes Then
+                SetStatus("Checking directory...", 0, ProgressBarStyle.Marquee)
+                If IO.Directory.Exists(TbDirectory.Text) = True And TbDirectory.Text.Count > 2 Then
+                    Dim processedFiles As Integer = 0, renamedFiles As Integer = 0
+                    For Each file In fileList
+                        If Not GetNewFilename(file) Is Nothing Then
+                            Try
+                                IO.File.Move(IO.Path.Combine(TbDirectory.Text, file), IO.Path.Combine(TbDirectory.Text, GetNewFilename(file)))
+                                renamedFiles += 1
+                            Catch ex As Exception
+                                Dim message As String
+                                If ex.InnerException Is Nothing Then
+                                    message = "There was an error: " & vbNewLine & ex.ToString
+                                Else
+                                    message = "There was an error: " & vbNewLine & ex.ToString & vbNewLine & ex.InnerException.ToString
+                                End If
+                                Select Case MessageBox.Show(Me, message, "Error", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1)
+                                    Case DialogResult.Abort
+                                        SetStatus("Aborted!", 0)
+                                        Exit Sub
+                                    Case DialogResult.Retry
+                                        Try
+                                            IO.File.Move(IO.Path.Combine(TbDirectory.Text, file), IO.Path.Combine(TbDirectory.Text, GetNewFilename(file)))
+                                            renamedFiles += 1
+                                        Catch ex2 As Exception
+                                            Dim message2 As String
+                                            If ex.InnerException Is Nothing Then
+                                                message2 = "There was an error: " & vbNewLine & ex.ToString
+                                            Else
+                                                message2 = "There was an error: " & vbNewLine & ex.ToString & vbNewLine & ex.InnerException.ToString
+                                            End If
+                                            If MessageBox.Show(Me, message & vbNewLine & vbNewLine & "Continuing with next file.", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1) = DialogResult.Cancel Then
+                                                SetStatus("Aborted!", 0)
+                                                Exit Sub
+                                            End If
+                                        End Try
+                                End Select
+                            End Try
+                        End If
+                        processedFiles += 1
+                        SetStatus("Renaming (" & processedFiles & " of " & fileList.Count & ") ...", processedFiles, fileList.Count)
+                    Next
+                    RefreshPreview(TbDirectory, New EventArgs)
+                    SetStatus("Renamed " & renamedFiles & "of " & processedFiles, processedFiles)
+                Else
+                    SetStatus("Directory doesn't exist!", 0, ProgressBarStyle.Blocks)
+                    TbDirectory.ForeColor = Color.Red
+                    Exit Sub
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub RefreshPreview(sender As Object, e As EventArgs) Handles TbDirectory.TextChanged, TbLookupPattern.TextChanged, TbReplacePattern.TextChanged
+        LvPreview.Items.Clear()
+
+        If sender Is TbDirectory Then
+            If IO.Directory.Exists(TbDirectory.Text) = True And TbDirectory.Text.Count > 2 Then
+                TbDirectory.ForeColor = SystemColors.WindowText
+                SetStatus("Scanning directory...", 0, ProgressBarStyle.Marquee)
+                fileList.Clear()
+                For Each file In IO.Directory.GetFiles(TbDirectory.Text)
+                    fileList.Add(file)
+                Next
+            Else
+                SetStatus("Directory doesn't exist!", 0, ProgressBarStyle.Blocks)
+                TbDirectory.ForeColor = Color.Red
+                Exit Sub
+            End If
+        End If
+
+        SetStatus("Refreshing preview...", 0, ProgressBarStyle.Marquee)
+        For Each file In fileList
+            Dim lvItem As ListViewItem
+            lvItem = LvPreview.Items.Add(IO.Path.GetFileName(file))
+            Try
+                lvItem.SubItems.AddRange(New String() {GetNewFilename(file)})
+            Catch ex As Exception
+                lvItem.SubItems.AddRange(New String() {IO.Path.GetFileName(file)})
+            End Try
+
+        Next
+        SetStatus("Refreshed preview!", 0, ProgressBarStyle.Blocks)
+    End Sub
+
+    Private Function GetNewFilename(oldFilename As String) As String
+        Dim result As String = Nothing
+        Dim lookupPattern As String = TbLookupPattern.Text
+        Dim replacePattern As String = TbReplacePattern.Text
+        Dim regexMC As Text.RegularExpressions.MatchCollection = System.Text.RegularExpressions.Regex.Matches(oldFilename, lookupPattern)
+        If regexMC.Count > 0 Then
+            result = regexMC(0).Result(replacePattern)
+        End If
+        Return result
+    End Function
+
+#Region "Status"
+    ''' <summary>
+    ''' Set the status and progress
+    ''' </summary>
+    ''' <param name="statusText"></param>
+    ''' <param name="progress"></param>
+    ''' <param name="style"></param>
+    Public Sub SetStatus(statusText As String, progress As Integer, Optional style As ProgressBarStyle = ProgressBarStyle.Blocks)
+        TsLblStatus.Text = statusText
+        TsPbStatus.Style = style
+        TsPbStatus.Value = progress
+    End Sub
+
+    ''' <summary>
+    ''' Set the status and progress
+    ''' </summary>
+    ''' <param name="statusText"></param>
+    ''' <param name="progress"></param>
+    ''' <param name="maxProgress"></param>
+    ''' <param name="style"></param>
+    Public Sub SetStatus(statusText As String, progress As Integer, maxProgress As Integer, Optional style As ProgressBarStyle = ProgressBarStyle.Blocks)
+        TsLblStatus.Text = statusText
+        TsPbStatus.Maximum = maxProgress
+        TsPbStatus.Style = style
+        TsPbStatus.Value = progress
+    End Sub
+#End Region
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        MessageBox.Show(Me, "RegexRenamer" & vbNewLine & "Version " & Application.ProductVersion & vbNewLine & "made by Mario Wagenknecht" & vbNewLine & vbNewLine & "Get more information at GitHub!", "About", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1)
+    End Sub
+
+    Private Sub LicenseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LicenseToolStripMenuItem.Click
+        MessageBox.Show(Me, "MIT License" & vbNewLine & vbNewLine & "Copyright (c) 2016 Mario Wagenknecht" & vbNewLine & vbNewLine & "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:" & vbNewLine & vbNewLine & "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software." & vbNewLine & vbNewLine & "THE SOFTWARE IS PROVIDED ""As Is"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.", "License", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1)
+    End Sub
+End Class
